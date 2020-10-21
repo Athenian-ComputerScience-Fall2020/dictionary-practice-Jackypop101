e0215cd10ba05c49ab14b46fb580de563320d2e1
Jackypop101 <23hchen@athenian.org>
2020-10-21 00:12:03 +0000

    Finished!!!!

--- my_code.py
+++ my_code.py
@@ -7,23 +7,21 @@ def make_dict():
     keys = ['Ten', 'Twenty', 'Thirty']
     values = [10, 20, 30]
     '''
-    currency =          # complete this line
+    currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}       # complete this line
 
     return currency
 
 def add_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # add a key value pair 'dairy': 'yogurt' to the following dictionary
-
     # add code here
-
+    foods['dairy'] = 'yogurt'
     return foods
 
 def remove_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # remove 'veggie': 'carrot' from the dictionary
-
-
+    del foods["veggie"]
     return foods
 
 def merge_dict():
@@ -31,14 +29,14 @@ def merge_dict():
     dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
     dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
     # add code here
-
-    return     # return new dictionary
+    dict3 = {**dict1, **dict2}
+    return dict3   # return new dictionary
 
 def access_key():
     # return the value of the key 'Twenty'
     currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
-    val =             # add code to assign the desired value to 'val'
+    val = currency.pop('Twenty')           # add code to assign the desired value to 'val'
     return val
 
 
@@ -48,5 +46,5 @@ def access_key():
 if __name__ == '__main__':
     # Test your code with this first
     # Change the function to test different sections
-    print(make_dict())
+    print(merge_dict())
 
